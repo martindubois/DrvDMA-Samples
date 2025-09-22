f31bf7d659147e986881bdf22b0898112e890baf
Martin Dubois <mdubois@kms-quebec.com>
2025-09-22 16:25:34 -0400

    U_XDMA_H2C

---
+++ U_XDMA_H2C/U_XDMA_H2C.cpp
@@ -0,0 +1,116 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2025 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   DrvDMA
+// File      Sample_U_Simple/Sample_U_Simple.cpp
+
+#define _KMS_WINDOWS_
+
+// ===== Windows ============================================================
+#include <Windows.h>
+
+// ===== DrvDMA =============================================================
+#include <DrvDMA_U.h>
+
+// Configurations
+// //////////////////////////////////////////////////////////////////////////
+
+// CURRENT LIMITATION  DrvDMA currently limit transfer size to 32 MiB.
+#define BUFFER_SIZE_byte (16 * 1024 * 1024)
+
+#define HARDWARE_ADDRESS (0x0000000000000000)
+
+// Entry point
+// //////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    // Step 1 - Create an instance of the DrvDMA class
+    auto lDD = DrvDMA::Create();
+
+    // Step 2 - Configure the DMA channel
+    // This sample configure the first H2C channel of a XDMA engine.
+    // a. By setting mDescQty to 65536 we indicate to the driver to allocate
+    //    space for 65536 descriptor. This is the maximum for a XDMA channel.
+    //    We could indicate a smaller value to save kernel memory if the
+    //    transfer are small.
+    // b. By setting mFlags.mExclusize to true, we indicate to the driver to
+    //    do not accept transfer request from other application.
+    // c. By setting mFlags.mKernel to true, we indicate to the driver that
+    //    it controls the DMA transfer.
+    // d. By setting mFlags.mToDevice to true, we indicate to the drive that
+    //    we want to tranfer data from the host memory to the device (H2C).
+    // e. By setting mMem_Index to 1, we indicate to the driver to use the
+    //    second configured BAR to access XDMA registers.
+    // f. By setting mMem_Offset_byte to 0, we indicate to the driver to use
+    //    this offset as base for the XDMA channel registers (first H2C
+    //    channel)
+    DrvDMA_Channel_Config lCfg;
+
+    memset(&lCfg, 0, sizeof(lCfg));
+
+    lCfg.mDescQty          = 65536;
+    lCfg.mEngine           = DrvDMA_AMD_XDMA;
+    lCfg.mFlags.mExclusive = true;
+    lCfg.mFlags.mKernel    = true;
+    lCfg.mFlags.mToDevice  = true;
+    lCfg.mMem_Index        = 1;
+    lCfg.mMem_Offset_byte  = 0;
+
+    unsigned int lCI;
+
+    auto lRet = lDD->Channel_Config(lCfg, &lCI);
+    if (DrvDMA_RESULT_OK(lRet))
+    {
+        // The channel is now configured and the logical DMA channel index is
+        // in lCI. Note that even if we use the first channel of the XDMA
+        // engine, this index can be any integer value. We need to consider it
+        // as an handle to the DMA channel.
+
+        // Step 3 - Allocating data buffer
+        auto lBuffer = new uint8_t[BUFFER_SIZE_byte];
+
+        // Step 4 - Writting data into the buffer
+        memset(lBuffer, 0, BUFFER_SIZE_byte);
+
+        // Step 5 - Prepare the electronic to receive data
+        // Here, you must configure the user electronic present in the FPGA.
+        // See the method Memory_GetAddress and Memory_GetSize
+
+        // Step 6 - Starting the DMA transfer
+        // We could also use the Transfer method. This one start the transfer
+        // and wait for its completion in a single method call.
+        // There is some note about the arguments:
+        // 1. lCI is the logical index of the channel to use
+        // 2. false indicate the transfer is from the host memory to the
+        //    device (H2C)
+        // 3. 0 indicate to take data directly at the start of the buffer.
+        // 4. HARDWARE_ADDRESS is the address on the internal AXI bus where
+        //    the data must be written
+        // 5. BUFFER_SIZE_byte is the size of the DMA tranfer.
+        // 6. &lTS is the address of an instance of DrvDMA_Transfer_Status.
+        //    The method put information about the DMA transfer in it.
+        DrvDMA_Transfer_Status lTS;
+
+        lRet = lDD->Start(lCI, false, lBuffer, 0, HARDWARE_ADDRESS, BUFFER_SIZE_byte, &lTS);
+        if (DrvDMA_OK_PENDING == lRet)
+        {
+            // Step 7 - Wait for the completion of the DMA transfer
+            lRet = lDD->Wait(&lTS);
+        }
+
+        // Step 8 - Release the data buffer
+        delete[] lBuffer;
+    }
+
+    if (DrvDMA_OK != lRet)
+    {
+        printf("Something got wrong (%s)\n", DrvDMA::GetResultName(lRet));
+    }
+
+    // Step 9 - Delete the DrvDMA instance
+    lDD->Delete();
+
+    return 0;
+}
